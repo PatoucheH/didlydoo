d0710a9d3e8963176e714e15a81e87aaa387cafb
Valher7 <Valentin.herman.pro@gmail.com>
2025-04-02 14:16:14 +0200

    resolve conflict main

--- src/module-json.js/get-json-info.js
+++ src/module-json.js/get-json-info.js
@@ -1,2 +1,14 @@
-/* */
+export function getInfo() {
+    return fetch("http://localhost:3000/api/events/")
+        .then(function(response) {
+            return response.json();
+        })
 
+        .then(function(data) {
+            return data;
+        })
+
+        .catch(function(error) {
+            console.log(error);
+        })
+};
