cb25277c6d204f81da898915d75bc5e2b8f774d1
NDC Studio <ndc.dev.code@gmail.com>
2025-04-02 16:33:15 +0200

    Show informations of all events in main page

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,35 +1,57 @@
-// Import the header
+/** @description -- Import the header */
 import { createHeader } from "./module-json.js/header.js";
+/** @description -- Import the .json data objects */
 import { getInfo } from "./module-json.js/get-json-info.js";
 
+/** @description -- Create the header of page */
 createHeader();
 
+/** @var {JSON[Object]} allInfo -- Contains the data of api in allInfo variable */
 let allInfo = await getInfo("/api/events");
 
-export function getAllInfos(data) {
+/**
+ * 
+ * @param {JSON[Object]} data 
+ * @description -- Iterate the array for display all events in db.json
+ */ 
+function getAllInfos(data) {
+    // Debug
     console.log(data);
     for (let i = 0; i < data.length; i++) {
-        let element = data[i];
-        const name = element.name;
-        const createdAt = element.created_at;
-        const description = element.description;
-        console.log(`${name}\n ${createdAt}\n ${description}`);
-        createEventCard(name, createdAt, description, i);     
+        let element = data[i]; // Ititialize the element in the variable
+        const name = element.name; // Catch element name
+        const createdAt = element.created_at; // Catch date od element was created
+        const description = element.description; // Catch the element description
+        const id = element.id;
+        console.log(`${name}\n ${createdAt}\n ${description}\n ${id}`); // Debug
+        createEventCard(name, createdAt, description, id); // Call the createdEventCard function
     }
 }
 
+/**
+ * 
+ * @param -- @var {JSON[Object]} allInfo
+ */
 getAllInfos(allInfo);
 
-function createEventCard(name, date, desc, i) {
-    const div = document.getElementById("container");
-    const newSection = document.createElement("section");
-    newSection.id = "event";
+/**
+ * 
+ * @param {string} name 
+ * @param {string} date 
+ * @param {string} desc 
+ * @param {string} id 
+ */
+function createEventCard(name, date, desc, id) {
+    const div = document.getElementById("container"); // Target the div container
+    const newSection = document.createElement("section"); // Create new <section>
+    newSection.id = "event"; // Add an id "event"
+    // Add event contains in the <section>
     newSection.innerHTML = `
-        <a href="./src/HTML/event.html" id="event-btn" id="event-info">
+        <a href="./src/HTML/event.html" id="${id}" id="event-info">
           <h3>${name}</h3>
           <h6 class="desc">${desc}</h6>
           <p>created date : ${date}</p>
         </a>
     `
-    div.appendChild(newSection);
+    div.appendChild(newSection); // Add <section> in <div> container
 }
