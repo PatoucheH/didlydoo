c9e6804f2474024883b1011b9cfcb94405e2aeff
NDC Studio <ndc.dev.code@gmail.com>
2025-04-02 14:53:28 +0200

    Merge dev

--- src/javascript/form.js
+++ src/javascript/form.js
@@ -12,6 +12,9 @@ addDate.addEventListener("click", (e) => {
   );
 });
 
+const formBtn = document.getElementById("submit");
+formBtn.addEventListener("click", getInputValue);
+
 export function getInputValue() {
   let inputObj = {};
   const name = document.getElementById("name-event");
---
+++ src/javascript/index.js
@@ -0,0 +1,7 @@
+import { getInputValue } from "./form.js";
+import { createHeader } from "./module-json.js/header.js";
+import { getInfo } from "./module-json.js/get-json-info.js";
+
+let rep = await getInfo("/api/events");
+
+createHeader();
