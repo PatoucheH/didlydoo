9b9756cbf1bf531747e8337143ac4ddb90da3432
Hugo Pat <Hugo.Paternoster@hotmail.com>
2025-04-03 09:35:16 +0200

    get dev change

--- src/javascript/event.js
+++
@@ -1,7 +0,0 @@
-
-import { displayEventById } from "./module-json.js/display-one-event.js";
-import { createHeader } from "./module-json.js/header.js";
-
-createHeader();
-displayEventById("38b643aeb883");
-
