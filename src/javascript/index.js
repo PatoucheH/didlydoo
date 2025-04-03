b749ef16450d5ea9a35a119df2c78ad99b85d103
pc-portable <martin.stephan9218@gmail.com>
2025-04-03 09:29:30 +0200

    reconstruct module

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,6 +2,12 @@
 import { getAllInfos } from "./module-json.js/get-all-infos.js";
 import { getInfo } from "./module-json.js/get-json-info.js";
 
+/** @description - Import the header */
+import { createHeader } from "./header.js";
+
+/** @description - Create the header of page */
+createHeader();
+
 /** @var {JSON[Object]} allInfo - Contains the data of api in allInfo variable */
 let allInfo = await getInfo("/api/events");
 
