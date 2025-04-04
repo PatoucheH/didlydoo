2c405def2b73b025a23543d34ab98c6767644d1d
NDC Studio <ndc.dev.code@gmail.com>
2025-04-04 14:25:24 +0200

    Finish CSS opptimized

--- src/javascript/module-json.js/create-header-tabel.js
+++ src/javascript/module-json.js/create-header-tabel.js
@@ -4,10 +4,9 @@ export function createHeader(dates, tabel) {
       firstHeader.classList.add("header");
       tabel.appendChild(firstHeader);
     
-      // create header dates, determines the number of columns grid
-      
+      // create header dates, determines the number of columns grid     
       tabel.style.gridTemplateColumns = `auto repeat(${dates.length}, 1fr)`;
-      tabel.style.width = "900px";
+      tabel.style.width = "80em";
       tabel.style.position = "relative";
       tabel.style.margin = "auto";
       dates.forEach((element) => {
