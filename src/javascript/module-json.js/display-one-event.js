0d9400099e32adcd4d713e0e0e7718b965b8045d
Hugo Pat <Hugo.Paternoster@hotmail.com>
2025-04-04 14:33:31 +0200

    .

--- src/javascript/module-json.js/display-one-event.js
+++ src/javascript/module-json.js/display-one-event.js
@@ -1,15 +1,8 @@
 import { getInfo } from "./get-json-info.js";
 import { modifyEvent } from "./modify-event.js";
 import { addDate } from "./add-date.js";
-import { createHeader } from "./create-header-tabel.js";
-import { getDates } from "./get-dates.js";
-import { createBody } from "./create-body-tabel.js";
-import { deleteEvent } from "./delete-event.js";
-import { createFormAttendances } from "./create-form-attendances.js";
-import { attendancesFormEvent } from "./attendance-form-event.js";
 
-export async function displayEventById(idEvent) {
-  let id = idEvent;
+export async function displayEventById(id) {
   let event = await getInfo(`/api/events/${id}`);
   let container = document.getElementById("event");
   container.innerHTML = `
@@ -17,25 +10,86 @@ export async function displayEventById(idEvent) {
       <h2>${event.name}</h2>
       <p>${event.description}</p>
       <p>by ${event.author}</p>
+      <div id="attendance-grid"></div>
     </section>
-    <div id="attendance-grid"></div>
   `;
 
   let tabel = document.getElementById("attendance-grid");
+
+  // create header blank
+  let firstHeader = document.createElement("div");
+  firstHeader.classList.add("header");
+  tabel.appendChild(firstHeader);
+
+  // create header dates, determines the number of columns grid
   let dates = event.dates;
+  tabel.style.gridTemplateColumns = `auto repeat(${dates.length}, 1fr)`;
+  tabel.style.width = "900px";
+  tabel.style.position = "relative";
+  tabel.style.margin = "auto";
+  let datesSave = [];
+  dates.forEach((element) => {
+    let header = document.createElement("div");
+    header.classList.add("header");
+    header.textContent = element.date;
+    tabel.appendChild(header);
+    datesSave.push(element.date);
+  });
 
-  let datesArray = getDates(dates);
-  createHeader(datesArray, tabel);
-
+  //participants attendances
   let attendances = await getInfo("/api/attendees/");
-  createBody(attendances, datesArray, tabel, id);
+
+  attendances.forEach((attendance) => {
+    //find the right event
+    let eventsUser = attendance.events.find((event) => event.id === id);
+
+    if (eventsUser) {
+      //create participant name row
+      let participant = document.createElement("div");
+      participant.classList.add("participant");
+      participant.textContent = attendance.name;
+      tabel.appendChild(participant);
+
+      let availabilities = {};
+      eventsUser.dates.forEach((d) => {
+        availabilities[d.date] = d.available;
+      });
+
+      //add availibilities for participant
+      datesSave.forEach((date) => {
+        let availability = document.createElement("div");
+        availability.classList.add("availability");
+
+        if (availabilities[date] !== undefined) {
+          availability.innerHTML = availabilities[date]
+            ? `<i class="fa-solid fa-thumbs-up"></i>`
+            : `<i class="fa-solid fa-thumbs-down"></i>`;
+        } else {
+          availability.textContent = "";
+        }
+        tabel.appendChild(availability);
+      });
+    }
+  });
 
   const deleteBtn = document.createElement("button");
   deleteBtn.id = "del-btn";
   deleteBtn.textContent = "Delete Event";
   container.appendChild(deleteBtn);
   deleteBtn.addEventListener("click", (e) => {
-    deleteEvent(id);
+    fetch(`http://localhost:3000/api/events/${id}/`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log("Succes ! ");
+        return response;
+      })
+      .catch((e) => {
+        console.error(e);
+      });
     location.href = "../../../index.html";
   });
 
@@ -58,29 +112,97 @@ export async function displayEventById(idEvent) {
     location.href = "./event.html";
   });
 
-  createFormAttendances(datesArray, container);
+  //modify attendances
+  let form = document.createElement("form");
+  form.action = "./event.html";
+  let newAttendance = document.createElement("div");
+  newAttendance.id = "attendance-input";
+  newAttendance.style.display = "grid";
+  newAttendance.style.gridTemplateColumns = `auto repeat(${dates.length}, 1fr)`;
+  form.appendChild(newAttendance);
 
+  let first = document.createElement("div");
+  first.classList.add("header");
+  newAttendance.appendChild(first);
+  datesSave.forEach((date) => {
+    let header = document.createElement("div");
+    header.classList.add("header");
+    header.textContent = date;
+    newAttendance.appendChild(header);
+  })
+
+  let newParticipant = document.createElement("div");
+  newParticipant.classList.add("participant");
+  let input = document.createElement("input");
+  input.type = "text";
+  input.placeholder = "Name";
+  newParticipant.appendChild(input);
+  newAttendance.appendChild(newParticipant);
+
+  datesSave.forEach((date) => {
+    let availability = document.createElement("div");
+    availability.id = `${date}`;
+    availability.classList.add("availability");
+    availability.innerHTML = `<input type="radio" id="${date}-true" name="${date}" value="true">
+                              <label for="${date}-true"><i class="fa-solid fa-thumbs-up"></i></label>
+                              <input type="radio" id="${date}-false" name="${date}" value="false">
+                              <label for="${date}-false"><i class="fa-solid fa-thumbs-down"></i></label>`;
+    newAttendance.appendChild(availability);
+  });
+  let submit = document.createElement("input");
+  submit.type = "submit";
+  submit.id = "new-attendance-submit";
+  submit.value = "Submit";
+  form.appendChild(submit);
+  container.appendChild(form);
   let submitForm = document.getElementById("new-attendance-submit");
   submitForm.addEventListener("click", (e) => {
-    e.preventDefault();
-    const input = document.getElementById("input-new-attendee");
-    const divParticipants = document.getElementById("attendance-grid");
-    const participants = divParticipants.querySelectorAll(".participant");
-    let participantsArray = [];
-    participants.forEach((el) => {
-      participantsArray.push(el.innerHTML);
+    // e.preventDefault();
+    let newParticipantObj = {};
+    let groupes = new Set();
+    document.querySelectorAll('input[type="radio"]').forEach((radio) => {
+      groupes.add(radio.name);
     });
+    let datesArray = [];
+    groupes.forEach((name) => {
+      let selections = {};
+      let selected = document.querySelector(`input[name="${name}"]:checked`);
+      if (selected) {
+        selections["date"] = name;
+        selections["available"] = selected.value === "true" ? true : false;
 
-    if (participantsArray.includes(input.value)) {
-      attendancesFormEvent(id, "POST");
-    } else {
-      attendancesFormEvent(id, "PATCH");
+        datesArray.push(selections);
+      }
+    });
+    newParticipantObj["name"] = input.value;
+    newParticipantObj["dates"] = datesArray;
+    console.log(newParticipantObj);
+    //POST to DB
+    try {
+      fetch(`http://localhost:3000/api/events/${id}/attend`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newParticipantObj),
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log("Succes:", data);
+
+          localStorage.setItem("event", data.id);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    } catch (e) {
+      console.log(e);
     }
-    location.href = "./event.html";
   });
 
   const divAddDate = document.createElement("div");
-  divAddDate.id = "div-add-date";
   divAddDate.innerHTML = `<input type="date" class="add-date-input">`;
   const addDateEvent = document.createElement("button");
   addDateEvent.textContent = "Add a date";
