f7e56b03b07f3b71ee6e4c5ec226a67c8f41d197
NDC Studio <ndc.dev.code@gmail.com>
2025-04-07 09:35:12 +0200

    Make a Jdoc form create-input.js & review indentations

--- src/javascript/event-function/create-input.js
+++ src/javascript/event-function/create-input.js
@@ -4,13 +4,17 @@
  * @description Create an input div with the information for the event to modify
  */
 export function createInputDiv(div) {
+  /** @const {HTMLElement} divToAdd - Create a element <div> */
   const divToAdd = document.createElement("div");
-  const html = `<input type="text" placeholder="Event name">\n
-                <input type="date" placeholder="Event date">\n
-                <input type="text" placeholder="Event author">\n
-                <input type="text" placeholder="Event desc">\n
-                `;
-
+  /** @const {string} html - Defines the HTML content for the event creation inputs */
+  const html = `
+    <input type="text" placeholder="Event name">\n
+    <input type="date" placeholder="Event date">\n
+    <input type="text" placeholder="Event author">\n
+    <input type="text" placeholder="Event desc">\n
+  `;
+  /** @description - Add html variable like content of divToAdd */
   divToAdd.innerHTML = html;
+  /** @description - Add divToAdd element ti div element */
   div.appendChild(divToAdd);
 }
