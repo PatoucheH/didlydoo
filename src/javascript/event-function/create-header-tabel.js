bb309741fdfb941d93ba74325677231535b206fb
NDC Studio <ndc.dev.code@gmail.com>
2025-04-07 09:31:00 +0200

    Make a Jdoc for create-header-tabel.js & review indentations

--- src/javascript/event-function/create-header-tabel.js
+++ src/javascript/event-function/create-header-tabel.js
@@ -5,20 +5,31 @@
  * @description - Create header of tabel with dates as titles
  */
 export function createHeader(dates, tabel) {
-  // create header blank
+  /** @var {HTMLElement} firstHeader - Create an element <div> */
   let firstHeader = document.createElement("div");
+  /** @description - Add "header" like a classList to firstHeader element */
   firstHeader.classList.add("header");
+  /** @description - Add firstHeader element to tabel element */
   tabel.appendChild(firstHeader);
 
-  // create header dates, determines the number of columns grid
+  /** @description - Add style display: grid; & determine the numbers of coloms by the length of dates array */
   tabel.style.gridTemplateColumns = `auto repeat(${dates.length}, 1fr)`;
+  /** @description - Add style width: 80em */
   tabel.style.width = "80em";
+  /** @description - Define relative positions  */
   tabel.style.position = "relative";
+  /** @description - Applies automatic margins to center the element in the grid layout */
   tabel.style.margin = "auto";
+
+  /** @description - For each elements of dates do... */
   dates.forEach((element) => {
+    /** @var {HTMLElement} header - Create an element <div> */
     let header = document.createElement("div");
+    /** @description - Add classList to header element */
     header.classList.add("header");
+    /** @description - Add the element like header element content */
     header.textContent = element;
+    /** @description - Add header element to tabel element */
     tabel.appendChild(header);
   });
 }
